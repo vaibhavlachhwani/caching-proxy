eefeb6a04f41622ffbfe679bf3ef0d73c67d781f
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-07-27 15:15:31 +0530

    create ConfigService

--- src/main/java/tech/vaibhavlachhwani/cachingproxy/service/ConfigService.java
+++ src/main/java/tech/vaibhavlachhwani/cachingproxy/service/ConfigService.java
@@ -1,4 +1,16 @@
 package tech.vaibhavlachhwani.cachingproxy.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConfigService {
+    private String originUrl;
+
+    public String getOriginUrl() {
+        return originUrl;
+    }
+
+    public void setOriginUrl(String originUrl) {
+        this.originUrl = originUrl;
+    }
 }
