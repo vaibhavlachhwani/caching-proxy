6d0f90faf490b5e05d490c6983991063da8595d0
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-07-27 12:50:06 +0530

    what is estonia up to now ...

---
+++ src/test/java/tech/vaibhavlachhwani/cachingproxy/CachingProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vaibhavlachhwani.cachingproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
