4bbb2409dc2938b3f6bf320d9bf3f7030e0d4fcc
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-07-27 15:33:26 +0530

    create cache controller

--- src/main/java/tech/vaibhavlachhwani/cachingproxy/controller/CacheController.java
+++ src/main/java/tech/vaibhavlachhwani/cachingproxy/controller/CacheController.java
@@ -1,4 +1,42 @@
 package tech.vaibhavlachhwani.cachingproxy.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.vaibhavlachhwani.cachingproxy.service.CacheService;
+import tech.vaibhavlachhwani.cachingproxy.service.ConfigService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cache")
 public class CacheController {
+    private ConfigService configService;
+    private CacheService cacheService;
+
+    public CacheController(ConfigService configService, CacheService cacheService) {
+        this.configService = configService;
+        this.cacheService = cacheService;
+    }
+
+    @PostMapping("/origin")
+    public ResponseEntity<?> setOrigin(@RequestBody Map<String, String> body) {
+        String originUrl = body.get("origin");
+
+        configService.setOriginUrl(originUrl);
+        return ResponseEntity.ok("Origin set to : " + originUrl);
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<?> clearCache() {
+        cacheService.clearCache();
+
+        return ResponseEntity.ok("Cache cleared.");
+    }
+
+    @GetMapping("/size")
+    public ResponseEntity<?> getCacheSize() {
+        int size = cacheService.getCacheSize();
+
+        return ResponseEntity.ok("Cache size : " + size);
+    }
 }
