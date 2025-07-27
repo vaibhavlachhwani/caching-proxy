8ea0582e4f8881a262dab49bd1a8e65900e80677
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-07-27 15:15:10 +0530

    create CacheService

--- src/main/java/tech/vaibhavlachhwani/cachingproxy/service/CacheService.java
+++ src/main/java/tech/vaibhavlachhwani/cachingproxy/service/CacheService.java
@@ -1,4 +1,24 @@
 package tech.vaibhavlachhwani.cachingproxy.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import tech.vaibhavlachhwani.cachingproxy.model.CachedRequest;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
 public class CacheService {
+    private ConcurrentHashMap<String, CachedRequest> cache= new ConcurrentHashMap<>();
+
+    public CachedRequest get(String url) {
+        return cache.get(url);
+    }
+
+    public void put(String url, ResponseEntity<String> response) {
+        cache.put(url, new CachedRequest(
+                response.getHeaders(),
+                response.getBody(),
+                response.getStatusCode()
+        ));
+    }
 }
